3542976bff7222d1e83da4a947ea56c089979567
MikaPham <kmpham804@gmail.com>
2018-12-04 12:16:54 +0700

    Testing Register/login

--- config.php
+++ config.php
@@ -1,7 +1,16 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   
+
+$dbServer = "localhost:3306";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "kankook";
+
+$db = new mysqli($dbServer, $dbUsername, $dbPassword, $dbName);
+if ($db->connect_errno)
+{
+	echo "Database connection failed: " . $db->connect_error;
+	exit();
+}
+
 ?>
